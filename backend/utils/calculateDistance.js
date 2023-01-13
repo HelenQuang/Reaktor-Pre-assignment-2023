646ce47fc47bcfd4133a43ad0017ca6f780ca2fc
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-13 21:52:22 +0200

    Create calculateDistance.js
    Algorithm to calculate distance between NDZ and drone position

---
+++ backend/utils/calculateDistance.js
@@ -0,0 +1,26 @@
+const calculateDistance = (dronePositionX, dronePositionY) => {
+  const ratio = 0.001; //ratio between real measurement and XYcoordinates
+  const NDZPositionX = 250000; //No Drone Zone original coordinate on X-axis
+  const NDZPositionY = 250000; //No Drone Zone original coordinate on Y-axis
+
+  //Difference between NDZ and drone position on X-axis
+  const differenceX = (NDZPositionX - dronePositionX) * ratio;
+
+  //Difference between NDZ and drone position on Y-axis
+  const differenceY = (NDZPositionY - dronePositionY) * ratio;
+
+  //Calculate distance between NDZ and drone position based on Pythagorean theorem
+  const distance = Math.sqrt(
+    Math.pow(differenceX, 2) + Math.pow(differenceY, 2)
+  );
+
+  console.log(distance);
+
+  if (distance <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+calculateDistance(73111, 223944);
