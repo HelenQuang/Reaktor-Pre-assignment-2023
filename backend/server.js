7c1a535646f778604295e39f157f7de6c2d7b821
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-16 16:47:33 +0200

    Update server.js
    Connect DB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,23 @@
-import app from "./app.js";
+const app = require("./app");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  });
 
 const port = 5000;
 app.listen(port, () => {
