160db61eb735761bbad3b8eaede7b421b504c1e7
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-17 01:47:21 +0200

    Update server.js
    Handle errors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-const app = require("./app");
-const mongoose = require("mongoose");
+import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-require("dotenv").config();
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! SHUTING DOWN...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config();
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -22,4 +29,12 @@ mongoose
 const port = 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! SHUTING DOWN...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
