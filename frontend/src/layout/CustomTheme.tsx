7680dbff0481f239ccffae925c218bdf91b6c1c8
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-18 00:50:21 +0200

    Create CustomTheme.tsx

---
+++ frontend/src/layout/CustomTheme.tsx
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Crimson Pro", "sans-serif"].join(","),
+  },
+});
+
+export default theme;
