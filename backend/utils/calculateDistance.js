1c5d61c99db6e6e4893acc628b6e117202a6d47a
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-18 16:28:01 +0200

    Update calculateDistance.js

--- backend/utils/calculateDistance.js
+++ backend/utils/calculateDistance.js
@@ -1,20 +1,20 @@
-export const calculateDistance = ({ dronePositionX, dronePositionY }) => {
+export const calculateDistance = (dronePositionX, dronePositionY) => {
   const ratio = 0.001; //ratio between real measurement and XYcoordinates
   const NDZPositionX = 250000; //No Drone Zone original coordinate on X-axis
   const NDZPositionY = 250000; //No Drone Zone original coordinate on Y-axis
 
   //Difference between NDZ and drone position on X-axis
-  const differenceX = (NDZPositionX - dronePositionX) * ratio;
+  const differenceX = (+NDZPositionX - +dronePositionX) * ratio;
 
   //Difference between NDZ and drone position on Y-axis
-  const differenceY = (NDZPositionY - dronePositionY) * ratio;
+  const differenceY = (+NDZPositionY - +dronePositionY) * ratio;
 
   //Calculate distance between NDZ and drone position based on Pythagorean theorem
   const distance = Math.sqrt(
     Math.pow(differenceX, 2) + Math.pow(differenceY, 2)
   );
 
-  return distance <= 100;
+  return { distance, isViolated: distance <= 100 };
 };
 
 export default calculateDistance;
