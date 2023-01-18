22cabde6736a418bc27c3deccc8848ad70701832
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-18 16:26:47 +0200

    Create infoModel.ts

---
+++ frontend/src/models/infoModel.ts
@@ -0,0 +1,11 @@
+interface infoModel {
+  _id: string;
+  droneSerialNumber: string;
+  distance: number;
+  pilotName: string;
+  pilotEmail: string;
+  pilotPhoneNumber: string;
+  observedAt: Date;
+}
+
+export default infoModel;
